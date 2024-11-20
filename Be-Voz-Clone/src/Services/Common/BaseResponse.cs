081503f7d3e0d836264028ca06a1d972ac220715
Tan Khoa <nguyentankhoa23082002@hotmail.com>
2024-11-21 04:58:16 +0700

    add base response

--- Be-Voz-Clone/src/Services/Common/BaseResponse.cs
+++ Be-Voz-Clone/src/Services/Common/BaseResponse.cs
@@ -1,4 +1,6 @@
-﻿using Be_Voz_Clone.src.Shared.Core.Enums;
+﻿using Be_Voz_Clone.src.Shared.Core.Common;
+using Be_Voz_Clone.src.Shared.Core.Enums;
+using System.Text.Json.Serialization;
 
 namespace Be_Voz_Clone.src.Services.Common
 {
@@ -6,6 +8,29 @@ namespace Be_Voz_Clone.src.Services.Common
     {
         public ResponseCode StatusCode { get; set; } = ResponseCode.OK;
 
-        public string? Message { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public ResultMessage? Message { get; set; }
+
+        public void AddMessage(string message)
+        {
+            Message = new ResultMessage("Success", message);
+        }
+
+        public void AddMessage(ResultMessage message)
+        {
+            Message = message;
+        }
+
+        public void AddError(string message)
+        {
+            StatusCode = ResponseCode.BADREQUEST;
+            Message = new ResultMessage("Error", message);
+        }
+
+        public void AddError(ResultMessage message)
+        {
+            StatusCode = ResponseCode.BADREQUEST;
+            Message = message;
+        }
     }
 }
